fb2ccfdab3cbd88f100b114b2e9791d761351864
Paolo <gentile.pao@gmail.com>
2018-12-10 16:42:10 +0100

    update duplicates

--- SeekDuplicatesAlgorithm.py
+++ SeekDuplicatesAlgorithm.py
@@ -28,17 +28,40 @@ class FindDuplicates:
 			base *= n
 		return base
 
-'''
+
+	'''
+	This second hash function is used for tackling the second part of the task. Instead of usic arithmetical operations, concatenating
+	the character values as strings assures that the position of each character is registered.
+	'''
+	def hash_it2(self, item, seed):
+		base = str(1099511628211) + str(seed)
+		for char in item:
+			n = self.wdict.get(char)
+			base = base + str(n)
+		return int(base)
+
+	'''
 	The check() function will compute the hash_it() function over the password exactly 3 times with 
 	different seeds. This function is also responsible of computing the modulo operation returning a value 
 	between 0 and the size of our bitarray. This value rapresents the index of the bitarray in which we'll check 
 	if the password is present or not. If the value of the bittarry is already set to 1 (False), it means that the word
 	has been already processed in a passed iteration and registered in the bitarry. Oterwise it means that no such word has been
 	found yet.'''
-	and then will compute the modulo operation
+
 	def check(self, item):
 		for i in range(0, len(self.list)):
 			position = self.hash_it(item, self.list[i]) % self.size
+			if self.bit_array[position] == False:
+				return False
+		return True
+
+	'''
+	This second check() function does exactly the same as above but using the second hash function, this function is used to check whether
+	a password has already been processed considering also the order of the characters in the password. i.e.: 'ABBA' != 'BAAB'
+	'''
+	def check2(self, item):
+		for i in range(0, len(self.list)):
+			position = self.hash_it2(item, self.list[i]) % self.size
 			if self.bit_array[position] == False:
 				return False
 		return True
@@ -50,6 +73,17 @@ class FindDuplicates:
 	def add(self, item):
 		pos = []
 		for i in range(0, len(self.list)):
-			d = self.hash_it(item, self.list[i]) % self.size
+			d = self.hash_it2(item, self.list[i]) % self.size
+			pos.append(d)
+			self.bit_array[d] = True
+
+
+	'''
+	Second add() function to be used for the second part of the task. It involves the usage of the second hash function hash_it2()
+	'''
+	def add2(self, item):
+		pos = []
+		for i in range(0, len(self.list)):
+			d = self.hash_it2(item, self.list[i]) % self.size
 			pos.append(d)
 			self.bit_array[d] = True
